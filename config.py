4cc36ad76ebfc88a8ff58d0323aceda0e5951945
MeG <112762788+MeG0302@users.noreply.github.com>
2025-09-08 00:26:50 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+ACCOUNT_CONFIGS = [
+    AccountConfig(
+        account_name="Account1",
+        api_key="your_account1_api_key_here",
+        secret_key="your_account1_secret_key_here"
+    ),
+    AccountConfig(
+        account_name="Account2",
+        api_key="your_account2_api_key_here",
+        secret_key="your_account2_secret_key_here"
+    )
+]
